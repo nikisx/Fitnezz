9aa1e1e4943ec96334305c39497f28a2b1478363
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2020-11-08 12:59:35 +0200

    Add Trainer controller and all trainers view

---
+++ Web/Fitnezz.Web.Web/Controllers/TrainersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fitnezz.Web.Web.Controllers
+{
+    public class TrainersController : Controller
+    {
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
