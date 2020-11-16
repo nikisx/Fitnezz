9a837d4aeae64ef6a27ef7144e267b112d2e6ab9
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2020-11-16 13:45:59 +0200

    Add view and action for creating trainers

---
+++ Web/Fitnezz.Web.Web.ViewModels/Trainers/TrainerCreateInputModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Fitnezz.Web.Web.ViewModels.Trainers
+{
+    public class TrainerCreateInputModel
+    {
+        [Required]
+        [MaxLength(30,ErrorMessage = "Max characters for the name are 30")]
+        [MinLength(6, ErrorMessage = "Min characters for the name are 6")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(18,55)]
+        public int Age { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Min characters for the specialty are 6")]
+        public string Specialty { get; set; }
+
+        [Required]
+        [Url]
+        public string Img { get; set; }
+
+    }
+}
--- Web/Fitnezz.Web.Web/Controllers/TrainersController.cs
+++ Web/Fitnezz.Web.Web/Controllers/TrainersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Fitnezz.Web.Web.ViewModels.Trainers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Fitnezz.Web.Web.Controllers
@@ -16,6 +17,22 @@ namespace Fitnezz.Web.Web.Controllers
         public IActionResult Hire(string id)
         {
             return RedirectToAction("All");
+        }
+
+        public IActionResult Create()
+        {
+            var viewModel = new TrainerCreateInputModel();
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TrainerCreateInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+            return this.View();
         }
 
         [HttpPost]
