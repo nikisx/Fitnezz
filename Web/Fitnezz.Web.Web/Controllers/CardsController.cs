b790c56f9db20e6b1880b3b3f4f74533a9dd236b
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2020-11-26 13:04:04 +0200

    Create cards controller and view

---
+++ Web/Fitnezz.Web.Web/Controllers/CardsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fitnezz.Web.Web.Controllers
+{
+    public class CardsController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
