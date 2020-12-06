dda403b8aefce459e0fbf14375e50d1e0e61a0e9
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2020-12-06 17:26:44 +0200

    Add view component for 3 random classes at home page

---
+++ Web/Fitnezz.Web.Web.ViewModels/ViewComponents/RandomClassViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Fitnezz.Web.Web.ViewModels.Classes;
+
+namespace Fitnezz.Web.Web.ViewModels.ViewComponents
+{
+    public class RandomClassViewModel : AllClassesViewModel
+    {
+        public string Title { get; set; }
+
+    }
+}
---
+++ Web/Fitnezz.Web.Web/ViewComponents/TakeRandomClassesViewComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Fitnezz.Web.Data.Common.Repositories;
+using Fitnezz.Web.Data.Models;
+using Fitnezz.Web.Web.ViewModels.ViewComponents;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fitnezz.Web.Web.ViewComponents
+{
+    public class TakeRandomClassesViewComponent : ViewComponent
+    {
+        private readonly IDeletableEntityRepository<Class> classesRepository;
+
+        public TakeRandomClassesViewComponent( IDeletableEntityRepository<Class> classesRepository)
+        {
+            this.classesRepository = classesRepository;
+        }
+
+        public IViewComponentResult Invoke(string title)
+        {
+
+            var classViewModel = this.classesRepository.All().Select(x=> new RandomClassViewModel()
+            {
+                Title = title,
+                Name = x.Name,
+                UsersCount = x.CardsClasses.Count,
+                DayOfWeek = x.DayOfWeek,
+                StartHour = x.StartingHour,
+                EndHour = x.FinishingHour,
+                TrainersName = x.TrainersClasses.Where(t=>t.ClassId == x.Id).Select(a=>a.Trainer.Name).ToList(),
+                Image = x.Image,
+            }).Take(3).ToList();
+
+            return this.View(classViewModel);
+        }
+    }
+}
