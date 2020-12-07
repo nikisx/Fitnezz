db1aa0768ed287c2dcfebab735b83fb7cc6d8e33
Nikolas Kostadinov <64860672+nikisx@users.noreply.github.com>
2020-12-08 00:29:14 +0200

    update ajax to add workout to dom without refreshing

--- Web/Fitnezz.Web.Web/wwwroot/js/addWorkoutAjax.js
+++ Web/Fitnezz.Web.Web/wwwroot/js/addWorkoutAjax.js
@@ -19,7 +19,13 @@
                     dataType: "text",
                     contentType: "application/json; charset=utf-8",
                     success: function () {
-                        location.reload();
+                        var newRowContent = "<tr class = \"row\"><td style=\"font-size: 20px; color: aliceblue\" class=\"col-md - 6 \">"+ workoutName + "</td>" +
+                            "<td style=\"font-size: 20px; color: aliceblue\" class=\"col-md - 6 \">0</td>" +
+                            "<td class=\"col-lg - 6\"><div class=\"button-holder\"><a href=\"/Workouts/All\" class=\"btn btn-warning\">Details</a></div></td></tr>" +
+                            "<td style=\"position: absolute; top:37%; left: 5%\">\r\n<input type=\"text\" name=\"username\" placeholder=\"Username\" style=\"width: 100px\" />\r\n<button type=\"submit\" class=\"btn btn-sm\"> Add to User</button></td>" +
+                            "<td style=\"position: absolute;right: 12%; top:36.6%;\"><button type=\"submit\" class=\"btn delete\" data-id=\"null\" data-controller=\"Workouts\" data-action=\"Delete\"data-body-message=\"Delete this workout?\">Delete</button></td>";
+
+                        $(newRowContent).prependTo($("#tblEntAttributes"));
                     },
                     failure: function(){
                         alert("Workout name should be between  5 and 30 characters");
